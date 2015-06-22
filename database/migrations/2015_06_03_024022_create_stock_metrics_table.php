d2c0695b1181a295194316fe93e2f2cf4e0f98f2
Wes Thorburn <westhorburn@gmail.com>
2015-06-22 18:45:31 +1000

    database table updates, migrations

--- database/migrations/2015_06_03_024022_create_stock_metrics_table.php
+++ database/migrations/2015_06_03_024022_create_stock_metrics_table.php
@@ -16,19 +16,19 @@ class CreateStockMetricsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->char('stock_code', 3);
-			$table->decimal('last_trade', 8, 2);
-			$table->double('average_daily_volume');
-			$table->double('EBITDA');
-			$table->decimal('earnings_per_share_current', 8, 2);
-			$table->decimal('earnings_per_share_next_year', 8, 2);
-			$table->decimal('price_to_earnings', 8, 2);
-			$table->decimal('price_to_book', 8, 2);
-			$table->decimal('year_high', 8, 2);
-			$table->decimal('year_low', 8, 2);
-			$table->decimal('fifty_day_moving_average', 8, 2);
-			$table->decimal('two_hundred_day_moving_average', 8, 2);
-			$table->string('market_cap');
-			$table->decimal('dividend_yield', 8, 2);
+			$table->decimal('last_trade', 8, 2)->nullable();
+			$table->double('average_daily_volume')->nullable();
+			$table->double('EBITDA')->nullable();
+			$table->decimal('earnings_per_share_current', 8, 2)->nullable();
+			$table->decimal('earnings_per_share_next_year', 8, 2)->nullable();
+			$table->decimal('price_to_earnings', 8, 2)->nullable();
+			$table->decimal('price_to_book', 8, 2)->nullable();
+			$table->decimal('year_high', 8, 2)->nullable();
+			$table->decimal('year_low', 8, 2)->nullable();
+			$table->decimal('fifty_day_moving_average', 8, 2)->nullable();
+			$table->decimal('two_hundred_day_moving_average', 8, 2)->nullable();
+			$table->string('market_cap')->nullable();
+			$table->decimal('dividend_yield', 8, 2)->nullable();
 			$table->timestamps();
 		});
 	}
