1e3701f05754c3c79411ba77cbe10019da0b3e40
Wes Thorburn <westhorburn@gmail.com>
2015-06-30 20:03:41 +1000

    fixed command schedule

--- app/Console/Commands/GetDailyFinancialsCommand.php
+++ app/Console/Commands/GetDailyFinancialsCommand.php
@@ -41,39 +41,36 @@ class GetDailyFinancialsCommand extends Command
     {
         if(Carbon::now()->isWeekDay()){
             $this->info("This process can take up to an hour...");
-            if($this->confirm('Do you wish to continue?'))
-            {
-                $this->info("Getting daily financials...");
-                $stockCodes = Stock::all()->lists('stock_code');
-                $numberOfStocks = count($stockCodes);
-                foreach($stockCodes as $key => $stockCode){
-                    $dailyFinancialsUrl = "http://real-chart.finance.yahoo.com/table.csv?s=".$stockCode.".AX&d=".(date('m')-1)."&e=".date('d')."&f=".date('Y')."&g=d&a=".(date('m')-1)."&b=".date('d')."&c=".date('Y')."&ignore=.csv";
-                    if(get_headers($dailyFinancialsUrl, 1)[0] == 'HTTP/1.1 200 OK'){
-                        file_put_contents('database/files/spreadsheet.txt', trim(str_replace("Date,Open,High,Low,Close,Volume,Adj Close", "", file_get_contents($dailyFinancialsUrl))));
-                        $spreadSheetFile = fopen('database/files/spreadsheet.txt', 'r');
-                        $dailyTradeRecords = array();
-                        while(!feof($spreadSheetFile)){
-                            $line = fgets($spreadSheetFile);
-                            $pieces = explode(',', $line);
-                            if(Carbon::createFromFormat('Y-m-d', $pieces[0])->isToday()){
-                                array_push($dailyTradeRecords, array(
-                                    'stock_code' => $stockCode,
-                                    'date' => $pieces[0],
-                                    'open' => $pieces[1],
-                                    'high' => $pieces[2],
-                                    'low' => $pieces[3],
-                                    'close' => $pieces[4],
-                                    'volume' => $pieces[5],
-                                    'adj_close' => $pieces[6]
-                                ));
-                            }
+            $this->info("Getting daily financials...");
+            $stockCodes = Stock::all()->lists('stock_code');
+            $numberOfStocks = count($stockCodes);
+            foreach($stockCodes as $key => $stockCode){
+                $dailyFinancialsUrl = "http://real-chart.finance.yahoo.com/table.csv?s=".$stockCode.".AX&d=".(date('m')-1)."&e=".date('d')."&f=".date('Y')."&g=d&a=".(date('m')-1)."&b=".date('d')."&c=".date('Y')."&ignore=.csv";
+                if(get_headers($dailyFinancialsUrl, 1)[0] == 'HTTP/1.1 200 OK'){
+                    file_put_contents('database/files/spreadsheet.txt', trim(str_replace("Date,Open,High,Low,Close,Volume,Adj Close", "", file_get_contents($dailyFinancialsUrl))));
+                    $spreadSheetFile = fopen('database/files/spreadsheet.txt', 'r');
+                    $dailyTradeRecords = array();
+                    while(!feof($spreadSheetFile)){
+                        $line = fgets($spreadSheetFile);
+                        $pieces = explode(',', $line);
+                        if(Carbon::createFromFormat('Y-m-d', $pieces[0])->isToday()){
+                            array_push($dailyTradeRecords, array(
+                                'stock_code' => $stockCode,
+                                'date' => $pieces[0],
+                                'open' => $pieces[1],
+                                'high' => $pieces[2],
+                                'low' => $pieces[3],
+                                'close' => $pieces[4],
+                                'volume' => $pieces[5],
+                                'adj_close' => $pieces[6]
+                            ));
                         }
-                        \DB::table('historicals')->insert($dailyTradeRecords);
                     }
-                    $this->info("Getting daily financials...".round(($key+1)*(100/$numberOfStocks), 2)."%");
+                    \DB::table('historicals')->insert($dailyTradeRecords);
                 }
-                $this->info("Finished getting daily financials for ".$numberOfStocks. " stocks.");
+                $this->info("Getting daily financials...".round(($key+1)*(100/$numberOfStocks), 2)."%");
             }
+            $this->info("Finished getting daily financials for ".$numberOfStocks. " stocks.");
         }
         else{
             $this->info("This command can only be run on weekdays");
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,6 +6,10 @@ Route::resource('search', 'SearchController');
 Route::get('graph/{dataType}/{stockCode}/{timeFrame}', 'StockController@graph');
 Route::resource('stock', 'StockController');
 
+Route::get('/test', function(){
+	echo "http://real-chart.finance.yahoo.com/table.csv?s=BGL.AX&d=".(date('m')-1)."&e=".date('d')."&f=".date('Y')."&g=d&a=".(date('m')-1)."&b=".date('d')."&c=".date('Y')."&ignore=.csv";
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
