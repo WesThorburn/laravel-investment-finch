2de63460d66505ba8aae6905dec6f6513ec259ff
Wes Thorburn <westhorburn@gmail.com>
2015-08-11 16:03:58 +1000

    Split getMarketStatus helper function in two, isTradingDay() is now a separate function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -9,24 +9,31 @@ function getCurrentTimeIntVal(){
 
 function getMarketStatus(){
 	if(getCurrentTimeIntVal() >= 100000 && getCurrentTimeIntVal() <= 160000){
-		if(Carbon::now()->isWeekDay()){
-			$marketClosedDays = ['2015-01-01','2015-01-26','2015-04-03','2015-04-06','2015-04-25','2015-06-08'];
-			if(!in_array(date("Y-m-d"), $marketClosedDays)){
-				if(date("Y-m-d") == "2015-12-24" || date("Y-m-d") == "2015-12-31"){
-					if(getCurrentTimeIntVal() <= 141000){
-						return "Market Open";
-					}
-					else{
-						return "Market Closed";
-					}
-				}
-				else{
+		if(isTradingDay()){
+			if(date("Y-m-d") == "2015-12-24" || date("Y-m-d") == "2015-12-31"){
+				if(getCurrentTimeIntVal() <= 141000){
 					return "Market Open";
 				}
+				else{
+					return "Market Closed";
+				}
+			}
+			else{
+				return "Market Open";
 			}
 		}
 	}
 	return "Market Closed";
+}
+
+function isTradingDay(){
+	if(Carbon::now()->isWeekDay()){
+		$marketClosedDays = ['2015-01-01','2015-01-26','2015-04-03','2015-04-06','2015-04-25','2015-06-08'];
+		if(!in_array(date("Y-m-d"), $marketClosedDays)){
+			return true;
+		}
+		return false;
+	}
 }
 
 function getServerTime(){
