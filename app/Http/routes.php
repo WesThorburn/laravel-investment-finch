6e0140c22474a17fee6d03757cda1d3a3a80f319
Wes Thorburn <westhorburn@gmail.com>
2015-08-12 14:57:13 +1000

    Fixed error, graph route was mistakenly placed inside the ajax route group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,10 +5,11 @@ Route::resource('stocks', 'StockController');
 Route::resource('search', 'SearchController');
 Route::resource('sectors', 'SectorController');
 
+Route::get('graph/{stockCode}/{timeFrame}/{dataType}', 'StockController@graph');
+
 Route::group(['prefix' => 'ajax'], function(){
 	Route::get('sectors/{sectorName}/daychanges', 'SectorController@sectorDayChanges');
 	Route::get('sectors/{sectorName}/otherstocksinsector', 'SectorController@otherStocksInSector');
-	Route::get('graph/{stockCode}/{timeFrame}/{dataType}', 'StockController@graph');
 	Route::get('relatedstocks/{stockCode}', 'StockController@relatedStocks');
 	Route::get('/marketstatus','SearchController@marketStatus');
 	Route::get('/marketchange', 'SearchController@marketChange');
