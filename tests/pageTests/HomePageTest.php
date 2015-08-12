dcb22b480a2d58e7222b383abf832ef303450171
Wes Thorburn <westhorburn@gmail.com>
2015-08-12 15:35:06 +1000

    modified home page test to include the best/worst performing stocks (30 days)

--- tests/pageTests/HomePageTest.php
+++ tests/pageTests/HomePageTest.php
@@ -4,6 +4,7 @@ class HomePageTest extends TestCase{
 	public function testIndexPage(){
 		$this->visit('/')
 			->see("Best Performing Sectors")->see("Worst Performing Sectors")->see("Best Performing Stocks ".date("Y"))
-			->see("Best Performing Stocks (7 Days)")->see("Worst Performing Stocks (7 Days)");
+			->see("Best Performing Stocks (7 Days)")->see("Worst Performing Stocks (7 Days)")
+			->see("Best Performing Stocks (30 Days)")->see("Worst Performing Stocks (30 Days)");
 	}
 }
