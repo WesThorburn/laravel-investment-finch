f2621f17866df1e1d483c386aabe3672acdd62ba
Wes Thorburn <wjthorburn@live.com.au>
2016-02-16 15:59:29 +1000

    Modified updateStockList command to delete non-existent stocks

--- app/Console/Commands/UpdateStockListCommand.php
+++ app/Console/Commands/UpdateStockListCommand.php
@@ -38,8 +38,17 @@ class UpdateStockListCommand extends Command {
 	 */
 	public function fire()
 	{
-		$companyListFromASX = UpdateStockListCommand::getCompanyList($this->option('testMode'));
+		$companyListFromASX = UpdateStockListCommand::getCompanyList();
+
+		if($this->option('testMode')){
+			//Test Mode file only contains TLS and CBA
+			$companyListFromASX = array_slice(explode(PHP_EOL, file_get_contents('database/files/testCompanyList.csv')), 3);
+		}
+
 		$numberOfStocks = count($companyListFromASX);
+		$existingStocksList = Stock::lists('stock_code');
+		$activeStocksList = [];
+
 		foreach($companyListFromASX as $key => $companyRow){
 			if($companyRow != null){
 				$stockCode = explode(',"', explode('",', $companyRow)[1])[0];
@@ -49,16 +58,17 @@ class UpdateStockListCommand extends Command {
 				    'sector' => substr(explode(',"', explode('",', $companyRow)[1])[1], 0, -2),
 				    'updated_at' => date("Y-m-d H:i:s")
 				]);
+				array_push($activeStocksList, $stockCode);
 				$this->info("Updating... ".round(($key+1)*(100/$numberOfStocks), 2)."%");
 			}
 		}
+		$inactiveStocks = array_diff($existingStocksList->toArray(), $activeStocksList);
+		foreach($inactiveStocks as $stockCode){
+			$stock = Stock::where('stock_code', $stockCode)->delete();
+		}
 		$this->info('The list of stocks was updated successfully!');
 	}
-	private static function getCompanyList($testMode){
-		if($testMode){
-			//Test Mode file only contains TLS and CBA
-			return array_slice(explode(PHP_EOL, file_get_contents('database/files/testCompanyList.csv')), 3);
-		}
+	private static function getCompanyList(){
 		return array_slice(explode(PHP_EOL, file_get_contents("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")), 3);
 	}
 	/**
