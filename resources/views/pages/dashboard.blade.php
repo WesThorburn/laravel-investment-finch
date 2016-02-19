37d6d2ecf67b6b23c37cc7bc28de18bf45c0901b
Wes Thorburn <wjthorburn@live.com.au>
2016-02-19 11:39:29 +1000

    Created table for discontinued stocks

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,10 +1,42 @@
 @extends('layouts.master')
 
 @section('body')
-	<div class="col-md-8 col-md-offset-2">
-		<h1>Dashboard</h1>
-		@foreach($discontinuedStocks as $stock)
-			<p>{{ $stock->stock_code }}</p>
-		@endforeach
+	<div class="col-md-6 col-md-offset-2">
+		<div class="panel panel-default">
+			<div class="panel-heading"><b>Discontinued Stocks</b></div>
+			<table class="table table-striped table-hover table-bordered table-condensed table-bordered-only-top-bottom no-margin-top" id="deleted_stocks">
+			    <thead>
+			        <tr>
+			            <th>Code</th>
+			            <th>Name</th>
+			            <th>Sector</th>
+			            <th>Deleted At</th>
+			        </tr>
+			    </thead>
+			    <tbody data-link="row" class="rowlink">
+				    @foreach($discontinuedStocks as $stock)
+						<tr>
+							<td>
+								{{ $stock->stock_code }}<a href="/stocks/{{$stock->stock_code}}"></a>
+							</td>
+							<td>{{ $stock->company_name }}</td>
+							<td>{{ $stock->sector }}</td>
+							<td>{{ $stock->deleted_at }}</td>
+						</tr>
+					@endforeach
+			    </tbody>
+			</table>
+		</div>
 	</div>
+
+	<script>
+		$(document).ready(function(){
+			$('#deleted_stocks').DataTable({
+				"dom": 'tp',
+				"pageLength": 20,
+				"lengthMenu": [20,50,100],
+				"stateSave": true
+			});
+		});
+	</script>
 @stop
