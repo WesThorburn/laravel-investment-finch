153c57e94985d09bf236b0156b103a7f814532cf
Wes Thorburn <wjthorburn@live.com.au>
2016-02-21 13:36:02 +1000

    Changed number_format of previous day's market cap

--- resources/views/pages/dashboard/market-cap-adjustments.blade.php
+++ resources/views/pages/dashboard/market-cap-adjustments.blade.php
@@ -34,7 +34,7 @@
 								{{ $stock->stock_code }}<a href="/stocks/{{$stock->stock_code}}"></a>
 							</td>
 							<td>{{ $stock->stock->company_name }}</td>
-							<td>{{ number_format($stock->yesterdays_market_cap) }}</td>
+							<td>{{ number_format($stock->yesterdays_market_cap, 2) }}</td>
 							<td>{{ $stock->market_cap }}</td>
 							<td>{{ $stock->last_trade }}</td>
 							<td>{{ $stock->volume }}</td>
