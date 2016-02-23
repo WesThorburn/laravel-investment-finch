a959ff3fdadf62acddb3f69de1024a0bd3e30de5
Wes Thorburn <wjthorburn@live.com.au>
2016-02-23 10:56:58 +1000

    Added migration to add yesterdays market cap to stock_metrics table

---
+++ database/migrations/2016_02_23_115437_add_yesterdays_market_cap_to_stock_metrics.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddYesterdaysMarketCapToStockMetrics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stock_metrics', function (Blueprint $table) {
+            $table->decimal('yesterdays_market_cap', 8, 2)->after('year_low');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stock_metrics', function (Blueprint $table) {
+            $table->dropColumn('yesterdays_market_cap');
+        });
+    }
+}
