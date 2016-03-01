a88520cd7dbc95db7a7ac388aea02d641b5088c9
Wes Thorburn <wjthorburn@live.com.au>
2016-03-01 16:42:31 +1000

    Added ajax datatable to adjustments dashboard page

--- resources/views/pages/dashboard/market-cap-adjustments.blade.php
+++ resources/views/pages/dashboard/market-cap-adjustments.blade.php
@@ -15,42 +15,19 @@
 			<li role="presentation" class="active"><a href="/dashboard/marketCapAdjustments">Market Cap Adjustments</a></li>
 		</ul>
 		<div class="panel panel-default single-pixel-top-margin">
-			<div class="panel-heading"><b>Stocks with Adjusted Market Caps (/1000)</b></div>
+			<div class="panel-heading"><b>All Stocks</b></div>
 			<table class="table table-striped table-hover table-bordered table-condensed table-bordered-only-top-bottom no-margin-top" id="adjusted_stocks">
 			    <thead>
 			        <tr>
 			            <th>Code</th>
-			            <th>Name</th>
-			            <th>Yesterday's Market Cap</th>
-			            <th>Current Market Cap</th>
-			            <th>Difference</th>
-			            <th>Stock Price</th>
-			            <th>Volume</th>
-			            <th></th>
+			            <th>Name</th>            
+			            <th>Day Change</th>
+			            <th>Mkt Cap (M)</th>
 			        </tr>
 			    </thead>
-			    <tbody data-link="row" class="rowlink">
-				    @foreach($marketCapAdjustments as $stock)
-						<tr>
-							<td>
-								{{ $stock->stock_code }}<a href="/stocks/{{$stock->stock_code}}"></a>
-							</td>
-							<td>{{ $stock->stock->company_name }}</td>
-							<td>{{ number_format($stock->yesterdays_market_cap, 2) }}</td>
-							<td>{{ $stock->current_market_cap }}</td>
-							<td>{{ number_format(($stock->current_market_cap-$stock->yesterdays_market_cap),2) }}</td>
-							<td>{{ $stock->last_trade }}</td>
-							<td>{{ $stock->volume }}</td>
-							<td>
-								{!! Form::open(['method' => 'post', 'action' => ['DashboardController@changeStockAdjustmentStatus', $stock->stock_code]]) !!}
-									{!! Form::hidden('adjustment', false) !!}
-									{!! Form::hidden('stockCode', $stock->stock_code) !!}
-									{!! Form::button("", ['name' => 'removeFromList', 'type' => 'submit', 'class' => 'glyphicon glyphicon-remove center-block', 'aria-hidden' => 'true']) !!}
-								{!! Form::close() !!}
-							</td>
-						</tr>
-					@endforeach
-			    </tbody>
+			    <div class="panel-body">
+			    	<tbody></tbody>
+			    </div>
 			</table>
 		</div>
 	</div>
@@ -69,15 +46,29 @@
 		</div>
 	</div>
 
-	<script>
-		$(document).ready(function(){
-			$('#adjusted_stocks').DataTable({
-				"dom": 'tp',
-				"pageLength": 20,
-				"lengthMenu": [20,50,100],
-				"stateSave": true,
-				"order": [[ 4, "asc" ]]
-			});
+<script>
+	$(document).ready(function(){
+		var stockTable = $('#adjusted_stocks').DataTable({
+			processing: true,
+			serverSide: true,
+			ajax: '/ajax/stocks/all',
+			lengthMenu: [20,50,100],
+			fnColumnCallback: function(nColumn){
+				console.log(nColumn);
+			},
+			columns: [
+				{data: 'stock_code', name: 'stocks.stock_code'},
+				{data: 'company_name', name: 'company_name'},
+	            {data: 'percent_change', name: 'percent_change', searchable: false}, 
+	            {data: 'current_market_cap', name: 'current_market_cap', searchable: false}, 
+			]
+		});		
+		//Make table rows clickable
+		$('#stock_table').delegate('tbody > tr', 'click', function(){
+			var data = stockTable.row(this).data();
+			window.location.assign('/stock/'+ data.stock_code)
 		});
-	</script>
+
+	});
+</script>
 @stop
