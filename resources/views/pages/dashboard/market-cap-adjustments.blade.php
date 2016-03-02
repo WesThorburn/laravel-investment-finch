27e22606a33c63b612bce4a063f8acc600eb6cd7
Wes Thorburn <wjthorburn@live.com.au>
2016-03-02 16:01:56 +1000

    Removed stock code add form on adjustments page

--- resources/views/pages/dashboard/market-cap-adjustments.blade.php
+++ resources/views/pages/dashboard/market-cap-adjustments.blade.php
@@ -35,49 +35,35 @@
 			</table>
 		</div>
 	</div>
-	<div class="col-md-3 col-md-offset-3">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<b>Add a Stock Code that requires a Market Cap Adjustment</b>
-			</div>
-			<div class="panel-body">
-				{!! Form::open(['action' => 'DashboardController@changeStockAdjustmentStatus', 'method' => 'post', 'class' => 'form-group form-inline']) !!}
-					{!! Form::hidden('adjustment', true) !!}
-					{!! Form::text('stockCode', null, ['class' => 'form-control', 'placeholder' => 'Add Stock Code']) !!}
-					{!! Form::submit("Add", ['class' => 'btn btn-default form-control']) !!}
-				{!! Form::close() !!}
-			</div>
-		</div>
-	</div>
 
-<script>
-	$(document).ready(function(){
-		var stockTable = $('#adjusted_stocks').DataTable({
-			processing: true,
-			serverSide: true,
-			ajax: '/dashboard/ajax/marketCapAdjustments',
-			lengthMenu: [20,50,100],
-			"order": [[ 4, "desc" ]],
-			fnColumnCallback: function(nColumn){
-				console.log(nColumn);
-			},
-			columns: [
-				{data: 'stock_code', name: 'stocks.stock_code'},
-				{data: 'company_name', name: 'company_name'},
-				{data: 'yesterdays_market_cap', name: 'yesterdays_market_cap', searchable: false},
-	            {data: 'current_market_cap', name: 'current_market_cap', searchable: false},
-	            {data: 'difference', name: 'difference', searchable: false},
-	            {data: 'percent_change', name: 'percent_change', searchable: false},
-	            {data: 'market_cap_requires_adjustment', name: 'market_cap_requires_adjustment'},
-	            {data: 'change_adjustment', name: 'change_adjustment'}
-			]
-		});		
-		//Make table rows clickable
-		$('#stock_table').delegate('tbody > tr', 'click', function(){
-			var data = stockTable.row(this).data();
-			window.location.assign('/stock/'+ data.stock_code)
+	<script>
+		$(document).ready(function(){
+			var stockTable = $('#adjusted_stocks').DataTable({
+				processing: true,
+				serverSide: true,
+				ajax: '/dashboard/ajax/marketCapAdjustments',
+				lengthMenu: [20,50,100],
+				"order": [[ 4, "desc" ]],
+				fnColumnCallback: function(nColumn){
+					console.log(nColumn);
+				},
+				columns: [
+					{data: 'stock_code', name: 'stocks.stock_code'},
+					{data: 'company_name', name: 'company_name'},
+					{data: 'yesterdays_market_cap', name: 'yesterdays_market_cap', searchable: false},
+		            {data: 'current_market_cap', name: 'current_market_cap', searchable: false},
+		            {data: 'difference', name: 'difference', searchable: false},
+		            {data: 'percent_change', name: 'percent_change', searchable: false},
+		            {data: 'market_cap_requires_adjustment', name: 'market_cap_requires_adjustment'},
+		            {data: 'change_adjustment', name: 'change_adjustment'}
+				]
+			});		
+			//Make table rows clickable
+			$('#stock_table').delegate('tbody > tr', 'click', function(){
+				var data = stockTable.row(this).data();
+				window.location.assign('/stock/'+ data.stock_code)
+			});
+
 		});
-
-	});
-</script>
+	</script>
 @stop
