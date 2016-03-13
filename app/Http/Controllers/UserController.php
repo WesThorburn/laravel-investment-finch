ea52df5b93c7d979d76282b2a3c70d6d5ebf556a
Wes Thorburn <wjthorburn@live.com.au>
2016-03-13 17:30:54 +1000

    Added basic user account page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.account');
     }
 
     /**
---
+++ resources/views/pages/account.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('nav')
+	@include('layouts.partials.nav-buttons', ['page' => 'account'])
+@stop
+
+@section('title')
+	Account
+@stop
+
+@section('body')
+	
+@stop
