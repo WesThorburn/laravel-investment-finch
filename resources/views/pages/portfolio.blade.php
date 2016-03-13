de7a3f6695c57f90a615ac0b092e3cf43e1ae134
Wes Thorburn <wjthorburn@live.com.au>
2016-03-13 17:26:55 +1000

    Added basic portfolio page

---
+++ resources/views/pages/portfolio.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('nav')
+	@include('layouts.partials.nav-buttons', ['page' => 'portfolio'])
+@stop
+
+@section('title')
+	Portfolio
+@stop
+
+@section('body')
+	
+@stop
