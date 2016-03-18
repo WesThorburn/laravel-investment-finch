180ea4df705e933e7d9c2a792e9038fd8c570735
Wes Thorburn <wjthorburn@live.com.au>
2016-03-18 12:33:28 +1000

    Added migration for creation of watchlists table

---
+++ database/migrations/2016_03_18_124132_create_watchlist_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWatchlistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watchlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('watchlist_name');
+            $table->timestamps();
+            $table->foreign('id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('watchlists');
+    }
+}
