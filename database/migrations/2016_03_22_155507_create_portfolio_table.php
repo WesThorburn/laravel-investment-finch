edc3315fb8b30b0fcb417dfc4af877148153bfcf
Wes Thorburn <wjthorburn@live.com.au>
2016-03-22 15:04:30 +1000

    Added a new migration to create portfolio table

---
+++ database/migrations/2016_03_22_155507_create_portfolio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfolioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('portfolio_name');
+            $table->timestamps();
+            $table->foreign('id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('portfolios');
+    }
+}
