402e9e316e94fd9e1f948a50974ca81111c4c647
Wes Thorburn <wjthorburn@live.com.au>
2016-03-29 18:00:43 +1000

    Fixed fatal error when trying to display portfolio page with no portfolios

--- resources/views/pages/user/portfolio.blade.php
+++ resources/views/pages/user/portfolio.blade.php
@@ -16,17 +16,23 @@
 					<div class="panel-heading">
 						Your Portfolios
 					</div>
-					<table class="table table-striped table-hover table-bordered table-condensed no-margins" id="portfolio_table">
-						<tbody data-link="row" class="rowlink">
-							@foreach($portfolios as $portfolio)
-								<tr @if($portfolio->id == $selectedPortfolio->id) class="table-row-active" @endif>
-									<td>
-										<a href="{{$portfolio->id}}">{{$portfolio->portfolio_name}}</a>
-									</td>
-								</tr>
-							@endforeach
-						</tbody>
-					</table>
+					@if($portfolios->first())
+						<table class="table table-striped table-hover table-bordered table-condensed no-margins" id="portfolio_table">
+							<tbody data-link="row" class="rowlink">
+								@foreach($portfolios as $portfolio)
+									<tr @if($portfolio->id == $selectedPortfolio->id) class="table-row-active" @endif>
+										<td>
+											<a href="{{$portfolio->id}}">{{$portfolio->portfolio_name}}</a>
+										</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
+					@else
+						<div class="panel-body">
+							You have no portfolios yet. 
+						</div>
+					@endif
 				</div>
 				<div class="panel panel-default">
 					<div class="panel-heading">
@@ -77,83 +83,85 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-9">
-				@include('layouts.partials.portfolio-stocks')
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Add a Stock to this Portfolio
-					</div>
-					<div class="panel-body">
-						<!-- Add Stock to Portfolio -->
-						<form role="form" action="{{action('PortfolioController@update', ['id' => $selectedPortfolio->id])}}" method="POST">
-							<input type="hidden" name="_method" value="put"/>
-							{{ csrf_field() }}
-							<div class="row">
-								<label class="col-xs-2 single-px-padding-right" for="stockCode">Stock Code</label>
-								<label class="col-xs-2 single-px-padding-left-right" for="purchasePrice">Purchase Price</label>
-								<label class="col-xs-2 single-px-padding-left-right" for="purchaseQty">Quantity</label>
-								<label class="col-xs-2 single-px-padding-left-right" for="brokerage">Brokerage</label>
-								<label class="col-xs-3 single-px-padding-left-right" for="date">Purchase Date</label>
-							</div>
-							<div class="row">
-								<div class="col-xs-2 single-px-padding-right">
-									<input name="stockCode" id="stockCode" type="text" class="form-control{{ $errors->has('stockCode') ? ' has-error' : ''}}" 
-									placeholder="Code" maxlength="3" value={{ old('stockCode') }}>
+			@if($portfolios->first())
+				<div class="col-md-9">
+					@include('layouts.partials.portfolio-stocks')
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							Add a Stock to this Portfolio
+						</div>
+						<div class="panel-body">
+							<!-- Add Stock to Portfolio -->
+							<form role="form" action="{{action('PortfolioController@update', ['id' => $selectedPortfolio->id])}}" method="POST">
+								<input type="hidden" name="_method" value="put"/>
+								{{ csrf_field() }}
+								<div class="row">
+									<label class="col-xs-2 single-px-padding-right" for="stockCode">Stock Code</label>
+									<label class="col-xs-2 single-px-padding-left-right" for="purchasePrice">Purchase Price</label>
+									<label class="col-xs-2 single-px-padding-left-right" for="purchaseQty">Quantity</label>
+									<label class="col-xs-2 single-px-padding-left-right" for="brokerage">Brokerage</label>
+									<label class="col-xs-3 single-px-padding-left-right" for="date">Purchase Date</label>
 								</div>
-								<div class="col-xs-2 single-px-padding-left-right">
-									<div class="input-group">
-										<div class="input-group-addon">$</div>
-										<input name="purchasePrice" id="purchasePrice" type="text" class="form-control{{ $errors->has('purchasePrice') ? ' has-error' : ''}}" value={{ old('purchasePrice') }}>
+								<div class="row">
+									<div class="col-xs-2 single-px-padding-right">
+										<input name="stockCode" id="stockCode" type="text" class="form-control{{ $errors->has('stockCode') ? ' has-error' : ''}}" 
+										placeholder="Code" maxlength="3" value={{ old('stockCode') }}>
+									</div>
+									<div class="col-xs-2 single-px-padding-left-right">
+										<div class="input-group">
+											<div class="input-group-addon">$</div>
+											<input name="purchasePrice" id="purchasePrice" type="text" class="form-control{{ $errors->has('purchasePrice') ? ' has-error' : ''}}" value={{ old('purchasePrice') }}>
+										</div>
+									</div>
+									<div class="col-xs-2 single-px-padding-left-right">
+										<input name="quantity" id="quantity" type="text" class="form-control{{ $errors->has('quantity') ? ' has-error' : ''}}" value={{ old('quantity') }}>
+									</div>
+									<div class="col-xs-2 single-px-padding-left-right">
+										<div class="input-group">
+											<div class="input-group-addon">$</div>
+											<input name="brokerage" id="brokerage" value="19.95" type="text" class="form-control{{ $errors->has('brokerage') ? ' has-error' : ''}}" value={{ old('brokerage') }}>
+										</div>
+									</div>
+									<div class="col-xs-3 single-px-padding-left-right">
+										<input name="date" id="date" type="date" class="form-control{{ $errors->has('date') ? ' has-error' : ''}}" value={{ old('date') }}>
+									</div>
+									<div class="col-xs-1 single-px-padding-left">
+										<button type="submit" class="btn btn-default">Add</button>
 									</div>
 								</div>
-								<div class="col-xs-2 single-px-padding-left-right">
-									<input name="quantity" id="quantity" type="text" class="form-control{{ $errors->has('quantity') ? ' has-error' : ''}}" value={{ old('quantity') }}>
-								</div>
-								<div class="col-xs-2 single-px-padding-left-right">
-									<div class="input-group">
-										<div class="input-group-addon">$</div>
-										<input name="brokerage" id="brokerage" value="19.95" type="text" class="form-control{{ $errors->has('brokerage') ? ' has-error' : ''}}" value={{ old('brokerage') }}>
+							</form>
+							@if($errors->has('stockCode') || $errors->has('purchasePrice') || $errors->has('quantity') || $errors->has('brokerage') || $errors->has('date'))
+								<div class="col-xs-12 default-margin-top">
+									<div class="alert alert-danger three-quarter-margin-bottom">
+										<ul>
+								            @foreach ($errors->all() as $error)
+								                <li>{{ $error }}</li>
+								            @endforeach
+								        </ul>
 									</div>
 								</div>
-								<div class="col-xs-3 single-px-padding-left-right">
-									<input name="date" id="date" type="date" class="form-control{{ $errors->has('date') ? ' has-error' : ''}}" value={{ old('date') }}>
+							@endif
+							@if(Session::has('addStockToPortfolioSuccess'))
+								<div class="col-xs-12 default-margin-top">
+									<div class="alert alert-success three-quarter-margin-bottom">
+										<ul>
+								            <li>{{ Session('addStockToPortfolioSuccess') }}</li>
+								        </ul>
+									</div>
 								</div>
-								<div class="col-xs-1 single-px-padding-left">
-									<button type="submit" class="btn btn-default">Add</button>
+							@elseif(Session::has('addStockToPortfolioError'))
+								<div class="col-xs-12 default-margin-top">
+									<div class="alert alert-danger three-quarter-margin-bottom">
+										<ul>
+								            <li>{{ Session('addStockToPortfolioError') }}</li>
+								        </ul>
+									</div>
 								</div>
-							</div>
-						</form>
-						@if($errors->has('stockCode') || $errors->has('purchasePrice') || $errors->has('quantity') || $errors->has('brokerage') || $errors->has('date'))
-							<div class="col-xs-12 default-margin-top">
-								<div class="alert alert-danger three-quarter-margin-bottom">
-									<ul>
-							            @foreach ($errors->all() as $error)
-							                <li>{{ $error }}</li>
-							            @endforeach
-							        </ul>
-								</div>
-							</div>
-						@endif
-						@if(Session::has('addStockToPortfolioSuccess'))
-							<div class="col-xs-12 default-margin-top">
-								<div class="alert alert-success three-quarter-margin-bottom">
-									<ul>
-							            <li>{{ Session('addStockToPortfolioSuccess') }}</li>
-							        </ul>
-								</div>
-							</div>
-						@elseif(Session::has('addStockToPortfolioError'))
-							<div class="col-xs-12 default-margin-top">
-								<div class="alert alert-danger three-quarter-margin-bottom">
-									<ul>
-							            <li>{{ Session('addStockToPortfolioError') }}</li>
-							        </ul>
-								</div>
-							</div>
-						@endif
+							@endif
+						</div>
 					</div>
 				</div>
-			</div>
+			@endif
 		</div>
 	</div>
 @stop
