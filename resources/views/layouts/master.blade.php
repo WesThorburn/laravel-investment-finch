3afbf09db2ba9bfdea7db51529ad1fb3eaa2e5d3
Wes Thorburn <wjthorburn@live.com.au>
2016-04-08 14:25:47 +1000

    Fix footer to bottom of page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,59 +40,61 @@
         		$('#marketChange').load('/ajax/marketchange');
         	}, 15000); 
 		</script>
-		<div id="container">
-			<nav class="navbar navbar-default navbar-ceiling">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-9">
-							<a href="/" class="pull-left default-margin-right"><img src="{{asset('../images/logo.png')}}" width="75" height="75"></a>
-							<h1 class="main-header">Investment Finch</h1>
-						</div>
-						<div class="col-md-3">
-							<ul class="nav navbar-nav pull-right">
-								<li>
-									@if(Auth::check())
-										<button onclick="redirect('/auth/logout')" class="btn btn-default btn-logout" id="logout">Logout</button>
-										@if(Auth::user()->is_admin)
-											<button onclick="redirect('/dashboard/marketCapAdjustments')" class="btn btn-default btn-logout">Admin</button>
-										@endif
-									@else
-										<button onclick="redirect('/auth/login')" class="btn btn-default btn-login" id="login">Log In</button>
-										<button onclick="redirect('/auth/register')" class="btn btn-default btn-register" id="register">Register</button>
-									@endif
-								</li>
-							</ul>
-							<h2 class="sub-header pull-right quarter-margin-top">The ASX simplified.</h2>
-						</div>
-					</div>
-				</div>
-			</nav>
-			<nav class="navbar navbar-inverse no-margin-bottom">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							@yield('nav')
-						</div>
-					</div>
-				</div>
-			</nav>
-			<!-- Don't display market info on homepage -->
-			@if(Request::route()->uri() != '/')
-				<nav class="navbar navbar-default">
+		<div class="wrapper">
+			<div id="container">
+				<nav class="navbar navbar-default navbar-ceiling">
 					<div class="container">
 						<div class="row">
-							<div id="marketStatus" class="quarter-margin-top"></div>
-							<div id="marketChange" class="quarter-margin-top"></div>
-
+							<div class="col-md-9">
+								<a href="/" class="pull-left default-margin-right"><img src="{{asset('../images/logo.png')}}" width="75" height="75"></a>
+								<h1 class="main-header">Investment Finch</h1>
+							</div>
+							<div class="col-md-3">
+								<ul class="nav navbar-nav pull-right">
+									<li>
+										@if(Auth::check())
+											<button onclick="redirect('/auth/logout')" class="btn btn-default btn-logout" id="logout">Logout</button>
+											@if(Auth::user()->is_admin)
+												<button onclick="redirect('/dashboard/marketCapAdjustments')" class="btn btn-default btn-logout">Admin</button>
+											@endif
+										@else
+											<button onclick="redirect('/auth/login')" class="btn btn-default btn-login" id="login">Log In</button>
+											<button onclick="redirect('/auth/register')" class="btn btn-default btn-register" id="register">Register</button>
+										@endif
+									</li>
+								</ul>
+								<h2 class="sub-header pull-right quarter-margin-top">The ASX simplified.</h2>
+							</div>
 						</div>
 					</div>
 				</nav>
-			@endif
-			@yield('body')
+				<nav class="navbar navbar-inverse no-margin-bottom">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								@yield('nav')
+							</div>
+						</div>
+					</div>
+				</nav>
+				<!-- Don't display market info on homepage -->
+				@if(Request::route()->uri() != '/')
+					<nav class="navbar navbar-default">
+						<div class="container">
+							<div class="row">
+								<div id="marketStatus" class="quarter-margin-top"></div>
+								<div id="marketChange" class="quarter-margin-top"></div>
+
+							</div>
+						</div>
+					</nav>
+				@endif
+				@yield('body')
+			</div>
+			<div class="push"></div>
 		</div>
 		@yield('footer')
-
-		<footer class="footer main-footer">
+		<div class="footer">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3">
@@ -109,6 +111,6 @@
 					</div>
 				</div>
 			</div>
-		</footer>
+		</div>
 	</body>
 </html>
