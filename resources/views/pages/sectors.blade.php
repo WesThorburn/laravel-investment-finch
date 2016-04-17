681803a8451f7c5fd6c9f0bc02349f9034c4b896
Wes Thorburn <wjthorburn@live.com.au>
2016-04-17 13:52:35 +1000

    Re-arrange sectors page panels

--- resources/views/pages/sectors.blade.php
+++ resources/views/pages/sectors.blade.php
@@ -70,63 +70,61 @@
 
 	</script>
 	<div class="container">
-		
-			<div class="col-sm-4 col-md-4 no-padding-left">
-				<div id="allSectors">
-					@include('layouts.partials.all-sectors-day-change-display')
-				</div>
+		<div class="col-sm-4 col-md-4 no-padding-left">
+			<div id="allSectors">
+				@include('layouts.partials.all-sectors-day-change-display')
 			</div>
-			<div class="col-sm-8 col-md-8 no-padding-left">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="panel panel-default three-quarter-margin-bottom">
-							<div class="panel-heading">
-								<div class="btn-group btn-group-sm pull-center" role="group">
-									<button class="btn btn-default active" onclick="getMarketCapsGraphData('top_5')" id="top_5">Top 5</button>
-									<button class="btn btn-default" onclick="getMarketCapsGraphData('top_10')" id="top_10">Top 10</button>
-									<button class="btn btn-default" onclick="getMarketCapsGraphData('top_15')" id="top_15">Top 15</button>
-									<button class="btn btn-default" onclick="getMarketCapsGraphData('top_20')" id="top_20">Top 20</button>
-									<button class="btn btn-default" onclick="getMarketCapsGraphData('all')" id="all">All</button>
-								</div>
-							</div>
-							<div class="panel-body">
-								<div id="sector_stocks_div" class="pull-left">
-									@piechart('SectorStocks', 'sector_stocks_div')
-								</div>
+		</div>
+		<div class="col-sm-8 col-md-8 no-padding-left">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel panel-default three-quarter-margin-bottom">
+						<div class="panel-heading">
+							<div class="btn-group btn-group-sm pull-center" role="group">
+								<button class="btn btn-default active" onclick="getSectorGraphData('last_month', 'Cap')" id="last_month">30 Days</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('last_3_months', 'Cap')" id="last_3_months">3 Months</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('last_6_months', 'Cap')" id="last_6_months">6 Months</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('last_year', 'Cap')" id="last_year">12 Months</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('last_2_years', 'Cap')" id="last_2_years">2 Years</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('last_5_years', 'Cap')" id="last_5_years">5 Years</button>
+								<button class="btn btn-default" onclick="getSectorGraphData('all_time', 'Cap')" id="all_time">All</button>
 							</div>
 						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="panel panel-default three-quarter-margin-bottom">
-							<div class="panel-heading">
-								<div class="btn-group btn-group-sm pull-center" role="group">
-									<button class="btn btn-default active" onclick="getSectorGraphData('last_month', 'Cap')" id="last_month">30 Days</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('last_3_months', 'Cap')" id="last_3_months">3 Months</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('last_6_months', 'Cap')" id="last_6_months">6 Months</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('last_year', 'Cap')" id="last_year">12 Months</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('last_2_years', 'Cap')" id="last_2_years">2 Years</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('last_5_years', 'Cap')" id="last_5_years">5 Years</button>
-									<button class="btn btn-default" onclick="getSectorGraphData('all_time', 'Cap')" id="all_time">All</button>
-								</div>
+						<div class="panel-body">
+							<div id="sector_caps_div" class="pull-left">
+								@areachart('SectorCaps', 'sector_caps_div')
 							</div>
-							<div class="panel-body">
-								<div id="sector_caps_div" class="pull-left">
-									@areachart('SectorCaps', 'sector_caps_div')
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<div id="otherStocksInSector">
-							@include('layouts.partials.other-stocks-in-sector')
 						</div>
 					</div>
 				</div>
 			</div>
-		
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel panel-default three-quarter-margin-bottom">
+						<div class="panel-heading">
+							<div class="btn-group btn-group-sm pull-center" role="group">
+								<button class="btn btn-default active" onclick="getMarketCapsGraphData('top_5')" id="top_5">Top 5</button>
+								<button class="btn btn-default" onclick="getMarketCapsGraphData('top_10')" id="top_10">Top 10</button>
+								<button class="btn btn-default" onclick="getMarketCapsGraphData('top_15')" id="top_15">Top 15</button>
+								<button class="btn btn-default" onclick="getMarketCapsGraphData('top_20')" id="top_20">Top 20</button>
+								<button class="btn btn-default" onclick="getMarketCapsGraphData('all')" id="all">All</button>
+							</div>
+						</div>
+						<div class="panel-body">
+							<div id="sector_stocks_div" class="pull-left">
+								@piechart('SectorStocks', 'sector_stocks_div')
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div id="otherStocksInSector">
+						@include('layouts.partials.other-stocks-in-sector')
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @stop
