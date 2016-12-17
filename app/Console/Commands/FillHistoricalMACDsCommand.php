6311968a846c059be9c634e0d24042d87064c66f
WesThorburn <wjthorburn@live.com.au>
2016-12-17 18:01:38 +1000

    Fix bugs inside of FillHistoricalMACDs

--- app/Console/Commands/FillHistoricalMACDsCommand.php
+++ app/Console/Commands/FillHistoricalMACDsCommand.php
@@ -39,70 +39,69 @@ class FillHistoricalMACDsCommand extends Command
      */
     public function handle()
     {
-        if($this->confirm('This process may take several hours, do you wish to continue? [y|N]')){
-            $uniqueStockCodes = Stock::all()->lists('stock_code');
-            $numberOfStocks = count($uniqueStockCodes);
+        $uniqueStockCodes = Stock::all()->lists('stock_code');
+        $numberOfStocks = count($uniqueStockCodes);
 
-            foreach($uniqueStockCodes as $stockKey => $stockCode){
-                $this->info("Processing Stock Code: ".$stockCode." ".round(($stockKey+1)*(100/$numberOfStocks), 2)."%");
-                $historicalRecords = Historicals::where('stock_code', $stockCode)->where('date', '>', '2016-01-01')->orderBy('date', 'asc')->get();
-                foreach($historicalRecords as $key => $record){
-                    //populate signal line
-                        //take 9-day ema of MACD line
+        foreach($uniqueStockCodes as $stockKey => $stockCode){
+            $this->info("Processing Stock Code: ".$stockCode." ".round(($stockKey+1)*(100/$numberOfStocks), 2)."%");
 
-                    //MACD histogram
-                        //MACD line - signal line
+            $historicalRecords = Historicals::where('stock_code', $stockCode)
+                ->where('date', '>', '2016-01-01')
+                ->orderBy('date', 'asc')
+                ->get();
 
-                    $twelveDayMultiplier = (2/(12 + 1));
-                    $twentySixDayMultiplier = (2/(26 + 1));
-
-                    if($record->date == $historicalRecords->first()->date){ 
-                        //First Date, we need the SMA (if not already filled)
-                        /*$twelveDayRecords = Historicals::where('stock_code', $stockCode)
-                            ->orderBy('date', 'desc')
-                            ->where('date', '<', '2016-01-01')
-                            ->take(12)
-                            ->lists('close');
-                        if($twelveDayRecords->count() > 0){
-                            $record->twelveDayEMA = $twelveDayRecords->sum()/$twelveDayRecords->count();
-                        }
-
-                        $twentySixDayRecords = Historicals::where('stock_code', $stockCode)
-                            ->orderBy('date', 'desc')
-                            ->where('date', '<', '2016-01-01')
-                            ->take(26)
-                            ->lists('close');
-                        if($twentySixDayRecords->count() > 0){
-                            $record->twentySixDayEMA = $twentySixDayRecords->sum()/$twelveDayRecords->count();
-                        }*/
-                    }
-                    else{
-                        $record->twelveDayEMA = ($record->close - $historicalRecords[$key-1]->twelveDayEMA) * $twelveDayMultiplier + $historicalRecords[$key-1]->twelveDayEMA;
-
-                        $record->twentySixDayEMA = ($record->close - $historicalRecords[$key-1]->twentySixDayEMA) * $twentySixDayMultiplier + $historicalRecords[$key-1]->twentySixDayEMA;
-                    }
-
-                    Historicals::where(['stock_code' => $stockCode, 'date' => $record->date])->update(['macd_line' => $record->twelveDayEMA - $record->twentySixDayEMA]);
-                }
-
-                //Crunch signal line + update histogram
-                $signalLineRecords = Historicals::where('stock_code', $stockCode)->where('date', '>', '2016-01-01')->whereNotNull('macd_line')->orderBy('date', 'asc')->get();
-
-                $nineDayMultiplier = (2/(9 + 1));
-
-                foreach($signalLineRecords as $key => $record){
-                    if($record->date == $signalLineRecords->first()->date){
-                        $record->nineDayMacdEMA = $record->macd_line;
-                    }
-                    else{
-                        $record->nineDayMacdEMA = ($record->macd_line - $signalLineRecords[$key-1]->macd_line) * $nineDayMultiplier + $signalLineRecords[$key-1]->macd_line;
-                    }
-                    Historicals::where(['stock_code' => $stockCode, 'date' => $record->date])->update(['signal_line' => $record->nineDayMacdEMA, 'macd_histogram' => $record->macd_line - $record->nineDayMacdEMA]);
-                }
-
-                $this->line("Completed ".round(($stockKey+1)*(100/$numberOfStocks), 2)."% | Stock: ".$stockCode);
-                
+            //Fill MACD Line
+            foreach($historicalRecords as $record){
+                $record->macd_line = $record->twelve_day_ema - $record->twenty_six_day_ema;
+                $record->save();
             }
+
+            //Crunch signal line + update histogram
+            $signalLineRecords = Historicals::where('stock_code', $stockCode)
+                ->where('date', '>', '2016-01-01')
+                ->orderBy('date', 'asc')
+                ->skip(10)
+                ->limit(PHP_INT_MAX)
+                ->get();
+
+            //Get 9 Day SMA for first signal line point
+            $macdLineRecords = Historicals::where('stock_code', $stockCode)
+                ->where('date', '<', $signalLineRecords->first()->date)
+                ->orderBy('date', 'DESC')
+                ->take(9)
+                ->lists('macd_line');
+
+            if($macdLineRecords->count() > 0){
+                $signalLineRecords->first()->signal_line = $macdLineRecords->sum()/$macdLineRecords->count();
+            }
+            else{
+                $signalLineRecords->first()->signal_line = $signalLineRecords->first()->close;
+            }
+            $signalLineRecords->first()->save();
+
+            $nineDayMultiplier = (2/(9 + 1));
+
+            foreach($signalLineRecords as $key => $record){
+                if($key > 0){
+                    $record->signal_line = ($record->macd_line - $signalLineRecords[$key-1]->macd_line) * $nineDayMultiplier + $signalLineRecords[$key-1]->macd_line;
+                    $record->save();
+                }
+            }
+
+            //Calculate Histogram
+            $histogramRecords = Historicals::where('stock_code', $stockCode)
+                ->where('date', '>', '2016-01-01')
+                ->orderBy('date', 'asc')
+                ->skip(10)
+                ->limit(PHP_INT_MAX)
+                ->get();
+
+            foreach($histogramRecords as $record){
+                $record->macd_histogram = $record->macd_line - $record->signal_line;
+                $record->save();
+            }
+
+            $this->line("Completed ".round(($stockKey+1)*(100/$numberOfStocks), 2)."% | Stock: ".$stockCode);
         }
     }
 }
