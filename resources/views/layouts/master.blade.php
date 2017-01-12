bb6ec0f637b700e65da71ea08b27f160226cdb8a
WesThorburn <wjthorburn@live.com.au>
2017-01-12 12:39:40 +1000

    Add adsense script to master page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -129,5 +129,12 @@
 				</div>
 			</div>
 		@endif
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		  (adsbygoogle = window.adsbygoogle || []).push({
+		    google_ad_client: "ca-pub-7363637623148655",
+		    enable_page_level_ads: true
+		  });
+		</script>
 	</body>
 </html>
