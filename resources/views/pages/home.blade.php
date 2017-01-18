9bb4e1f1805042717a5f532799bf018730d77d5b
WesThorburn <wjthorburn@live.com.au>
2017-01-19 08:26:58 +1000

    Add adsense test code to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -78,6 +78,19 @@
 	</script>
 	<div class="container three-quarter-margin-top">
 		<div class="row">
+			<div class="col-md-12">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- test-leaderboard -->
+				<ins class="adsbygoogle"
+				     style="display:inline-block;width:728px;height:90px"
+				     data-ad-client="ca-pub-7363637623148655"
+				     data-ad-slot="2516836775"></ins>
+				<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-4 no-padding-right-md">
 				<div class="panel panel-default three-quarter-margin-bottom">
 					<div class="panel-heading"><h3 class="no-margin-top no-margin-bottom text-center" id="marketOpenClosed"></h3></div>
