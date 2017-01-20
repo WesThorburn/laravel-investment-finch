b39379f203762376a13da9b864e5da54d91e0409
WesThorburn <wjthorburn@live.com.au>
2017-01-20 19:32:40 +1000

    Fix bootstrap class bug

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -125,7 +125,7 @@
 					@yield('body')
 					<div class="push"></div>
 				</div>
-				<div class="col-md-2 no-padding-left no-padding-right visible-md visible-md-* visible-lg-*">
+				<div class="col-md-2 no-padding-left no-padding-right visible-md-* visible-lg-*">
 					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 						<!-- test-responsive -->
 						<ins class="adsbygoogle"
